b031b03407d6470e84bc7f93d87cc486a4fabdf2
Pedro Henrique de Almeida Alonso ALMEIDA ALONSO <phaalonso@gmail.com>
2020-01-19 00:25:16 -0300

    Web

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -7,8 +7,9 @@ const parseStringAsArray = require('../utils/parseStringAsArray');
 module.exports = {
     async index (request, response) {
         const devs = await Dev.find();
+        console.log(devs);
         return response.json(devs);
-    },
+    },  
     async store (request, response) {
         const { github_username, techs, latitude, longitude } = request.body;
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://usuario1:pJQBER7NcKuQ7ZPt@omnistack-clnuj.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://usuario1:GuVHoC0Rj7DSiWjG@omnistack-clnuj.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
