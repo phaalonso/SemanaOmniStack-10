37e216b160cf2e16c22ec83a6a4604f107e051c1
Pedro Henrique de Almeida Alonso ALMEIDA ALONSO <phaalonso@gmail.com>
2020-01-20 00:39:14 -0300

    Funcionalidades a mais

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 // index -> findAll / show -> findById / store -> criar / update -> atualizar / destroy -> apagar
 
@@ -37,6 +38,17 @@ module.exports = {
                 techs: techsArray,
                 location,
             });
+
+            // Filtrar as conexões que estão no maximo 10km de distancia
+            // e que o novo dev tenha pelo menos uma das tecnologias filtradas
+
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray,
+            );
+
+            console.log('Near', sendSocketMessageTo);
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
         return response.json(dev);
     }
